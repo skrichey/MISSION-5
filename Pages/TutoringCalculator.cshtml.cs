ac5ecfe0e7f94ecb31fe20c88f26ea3e86551c54
Sam Richey <skr1501@byu.edu>
2025-02-07 12:37:58 -0700

    Add project files.

---
+++ Pages/TutoringCalculator.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MISSION_5.Pages
+{
+    public class TutoringCalculatorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ wwwroot/js/calculator.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    $("#calculate").click(function () {
+        let hours = $("#hours").val();
+        let rate = $("#rate").val();
+
+        if (hours <= 0 || isNaN(hours)) {
+            alert("Please enter a valid positive number.");
+            return;
+        }
+
+        let total = hours * rate;
+        $("#total").text(total.toFixed(2));
+    });
+});
